ebc2e7ae629f24f23e7aa19a09aecece3285118b
juan_lorza <lorza112@hotmail.com>
2023-04-26 21:53:42 -0500

    intento carrusel

--- etnia/src/components/banner/banner.tsx
+++ etnia/src/components/banner/banner.tsx
@@ -8,14 +8,18 @@ import img2 from "./assets/20221120_164145.jpg";
 import img3 from "./assets/20221120_164211.jpg";
 import img4 from "./assets/20221120_164047.jpg";
 import img5 from "./assets/20221120_164212.jpg";
+import img6 from "./assets/20221120_164100.jpg";
 import { BsFacebook, BsTwitter, BsInstagram } from "react-icons/bs";
+// import "swiper/swiper-bundle.min.css";
+// import "swiper/swiper.min.css";
 
 import "swiper/css";
+import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
-import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import { Pagination, EffectCoverflow } from "swiper";
 
-SwiperCore.use([Navigation, Pagination, Autoplay]);
+// SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 const prompt = Prompt({
   subsets: ["latin"],
@@ -28,56 +32,79 @@ function Banner() {
       <div className={styles.headercontainer}>
         <div className={styles.headercontainer__banner}>
           <Swiper
-            autoplay={{
-              delay: 2000,
-              disableOnInteraction: false,
+            // autoplay={{
+            //   delay: 3000,
+            //   disableOnInteraction: false,
+            // }}
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
             }}
-            slidesPerView={3}
-            spaceBetween={30}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Pagination]}
-            className={styles.headercontainer__banner__mySwiper}
+            pagination={true}
+            modules={[EffectCoverflow, Pagination]}
+            className="mySwiper"
+            // effect={"coverflow"}
+            // grabCursor={true}
+            // centeredSlides={true}
+            // slidesPerView={3}
+            // spaceBetween={30}
+            // coverflowEffect={{
+            //   rotate: 50,
+            //   stretch: 0,
+            //   depth: 100,
+            //   modifier: 2,
+            //   slideShadows: true,
+            // }}
+            // pagination={{
+            //   clickable: true,
+            // }}
+            // navigation={true}
+            // modules={[Pagination]}
+            // className={"mySwiper"}
           >
             <SwiperSlide
-              className={styles.headercontainer__banner__SwiperSlide}
+              key={1}
+              // className={styles.headercontainer__banner__SwiperSlide}
             >
-              <Image src={img1} alt="img logo" width={750} height={700} />
+              <Image src={img1} alt="img logo" width={600} height={550} />
             </SwiperSlide>
             <SwiperSlide
-              className={styles.headercontainer__banner__SwiperSlide}
+              key={2}
+              // className={styles.headercontainer__banner__SwiperSlide}
             >
-              <Image src={img2} alt="img logo" width={750} height={700} />
+              <Image src={img2} alt="img logo" width={600} height={550} />
             </SwiperSlide>
             <SwiperSlide
-              className={styles.headercontainer__banner__SwiperSlide}
+              key={3}
+              // className={styles.headercontainer__banner__SwiperSlide}
             >
-              <Image src={img3} alt="img logo" width={750} height={700} />
+              <Image src={img3} alt="img logo" width={600} height={550} />
             </SwiperSlide>
             <SwiperSlide
-              className={styles.headercontainer__banner__SwiperSlide}
+              key={4}
+              // className={styles.headercontainer__banner__SwiperSlide}
             >
-              <Image src={img4} alt="img logo" width={750} height={700} />
+              <Image src={img4} alt="img logo" width={600} height={550} />
             </SwiperSlide>
             <SwiperSlide
-              className={styles.headercontainer__banner__SwiperSlide}
+              key={5}
+              // className={styles.headercontainer__banner__SwiperSlide}
             >
-              <Image src={img5} alt="img logo" width={750} height={700} />
+              <Image src={img5} alt="img logo" width={600} height={550} />
             </SwiperSlide>
             <SwiperSlide
-              className={styles.headercontainer__banner__SwiperSlide}
+              key={6}
+              // className={styles.headercontainer__banner__SwiperSlide}
             >
-              Slide 6
+              <Image src={img6} alt="img logo" width={600} height={550} />
             </SwiperSlide>
-            <SwiperSlide
-              className={styles.headercontainer__banner__SwiperSlide}
-            >
-              Slide 7
-            </SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
           </Swiper>
         </div>
       </div>
